7855aa5e2e640fda165f09a3694f127f92dee251
SinghAshutosh03 <ashutoshsingh030302@gmail.com>
2025-01-22 23:17:07 +0530

    solution p2

---
+++ SimpleInterest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Principal amount: ");
+        double principal = input.nextDouble();
+
+        System.out.print("Enter the Rate of interest: ");
+        double rate = input.nextDouble();
+
+        System.out.print("Enter the Time in years: ");
+        double time = input.nextDouble();
+
+        double simpleInterest = (principal * rate * time) / 100;
+
+        System.out.println("The Simple Interest is " + simpleInterest + " for Principal " +
+                principal + ", Rate of Interest " + rate + "%, and Time " + time + " years.");
+    }
+}
