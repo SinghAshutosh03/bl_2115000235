9e73202de3341eac6c7595e23a4ae336a91ee5da
SinghAshutosh03 <ashutoshsingh030302@gmail.com>
2025-01-23 22:56:57 +0530

    sol23jan

---
+++ ArmstrongNum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ArmstrongNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int sum = 0, originalNumber = number;
+        while (originalNumber != 0) {
+            int remainder = originalNumber % 10;
+            sum += remainder * remainder * remainder;
+            originalNumber /= 10;
+        }
+        System.out.println(sum == number ? "Armstrong Number" : "Not an Armstrong Number");
+    }
+}
